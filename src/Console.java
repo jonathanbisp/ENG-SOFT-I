05acf652856eb684a10371b6c3a59a911917aeda
Arriverdecci <yuri.cardosovasconcelos@gmail.comm>
2022-11-23 21:24:53 -0300

    comand

--- src/Console.java
+++ src/Console.java
@@ -2,6 +2,14 @@ import java.util.HashMap;
 
 import books.Book;
 import comands.Comand;
+import comands.DevolverComand;
+import comands.EmprestarComand;
+import comands.LivroComand;
+import comands.NtfComand;
+import comands.ObservarComand;
+import comands.ReservarComand;
+import comands.SairComand;
+import comands.UsuarioComand;
 import users.User;
 import users.Professor;
 
@@ -9,6 +17,16 @@ public class Console {
 	
 	public HashMap<String, Comand> comandos;
 	
+	public void inicializarComandos() {
+		comandos.put("dev", new DevolverComand());
+		comandos.put("emp", new EmprestarComand());
+		comandos.put("liv", new LivroComand());
+		comandos.put("ntf", new NtfComand());
+		comandos.put("obs", new ObservarComand());
+		comandos.put("res", new ReservarComand());
+		comandos.put("sai", new SairComand());
+		comandos.put("usu", new UsuarioComand());
+	}
 	
 	
     public static void main(String[] args) {
